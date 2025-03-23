84da9b537f581752341b530348ee25b109d9c9df
배석현 <bjh4966@naver.com>
2025-03-23 20:16:27 +0900

    [FEAT] foodcontroller 생성

---
+++ src/main/java/capstone/mju/backend/domain/openapi/controller/FoodController.java
@@ -0,0 +1,22 @@
+package capstone.mju.backend.domain.openapi.controller;
+
+import capstone.mju.backend.domain.openapi.service.FoodNutritionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/foods")
+@RequiredArgsConstructor
+public class FoodController {
+
+    private final FoodNutritionService foodService;
+
+    @PostMapping("/fetch")
+    public ResponseEntity<String> fetchData() {
+        foodService.fetchAndSaveFoodData();
+        return ResponseEntity.ok("데이터 수집 완료");
+    }
+}
