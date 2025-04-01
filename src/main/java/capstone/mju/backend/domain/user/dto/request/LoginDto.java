a5f05b341bdf29a12f3956fdba5f1739fb9822c0
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-2.local>
2025-04-01 23:24:23 +0900

    feat: User Entity 및 Controller 설정 - #5

--- src/main/java/capstone/mju/backend/domain/user/dto/request/LoginDto.java
+++ src/main/java/capstone/mju/backend/domain/user/dto/request/LoginDto.java
@@ -1,2 +1,17 @@
-package capstone.mju.backend.domain.user.dto.request;public class LoginDto {
+package capstone.mju.backend.domain.user.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class LoginDto {
+    @NotBlank(message = "이메일이 비었습니다.")
+    @Pattern(regexp = "^[A-Za-z0-9]+@[A-Za-z0-9]+.[A-Za-z]{2,6}$", message = "이메일 형식이 맞지 않습니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호기 비어있습니다.")
+    private String password;
 }
