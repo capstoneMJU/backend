c86cc884eb33f61ac82ea17fee087f6bfadf6203
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-2.local>
2025-04-11 21:07:24 +0900

    feat: recipe field 생성 - #28

--- src/main/java/capstone/mju/backend/domain/recipe/domain/Recipe.java
+++ src/main/java/capstone/mju/backend/domain/recipe/domain/Recipe.java
@@ -1,4 +1,19 @@
 package capstone.mju.backend.domain.recipe.domain;
 
-public class Recipe {
+import capstone.mju.backend.domain.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "recipe")
+public class Recipe extends BaseEntity {
+    private String title;
+    private List<Ingredient> requiredIngredients;
+    private String recipeContent;
 }
