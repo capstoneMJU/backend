6112d7c250e777204c3de27b182157c3bdf7010f
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-2.local>
2025-04-11 23:36:05 +0900

    feat: Recipe api 관련 requestDto 작성 - #28

--- src/main/java/capstone/mju/backend/domain/recipe/dto/request/ScrapRecipeRequest.java
+++ src/main/java/capstone/mju/backend/domain/recipe/dto/request/ScrapRecipeRequest.java
@@ -1,2 +1,33 @@
-package capstone.mju.backend.domain.recipe.dto.request;public class ScrapRecipeRequest {
+package capstone.mju.backend.domain.recipe.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Schema(description = "레시피 생성 요청 DTO")
+public class ScrapRecipeRequest {
+    @NotBlank
+    @Schema(description = "스크랩할 레시피 제목", example = "맛있는 안창살 스테이크")
+    private String title;
+
+    @NotEmpty
+    @Schema(description = "필요한 재료 리스트")
+    private List<IngredientRequest> ingredients;
+
+    @NotBlank
+    @Schema(description = "레시피 단계 및 설명", example = "1. 고기를 굽는다\n2. 감자와 토마토를 굽는다")
+    private String steps;
+
+    @Getter
+    @Schema(description = "재료 정보 DTO")
+    public static class IngredientRequest {
+
+        @NotBlank
+        @Schema(description = "재료 이름", example = "감자")
+        private String name;
+    }
 }
