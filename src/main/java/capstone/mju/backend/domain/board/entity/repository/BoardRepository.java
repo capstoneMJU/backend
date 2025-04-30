484e914ff9087062ccf45e7f4e9b1872f7abcf61
배석현 <bjh4966@naver.com>
2025-05-01 03:40:00 +0900

    [FEAT] N+1 문제 해결

--- src/main/java/capstone/mju/backend/domain/board/entity/repository/BoardRepository.java
+++ src/main/java/capstone/mju/backend/domain/board/entity/repository/BoardRepository.java
@@ -5,6 +5,7 @@ import capstone.mju.backend.domain.board.entity.Category;
 import capstone.mju.backend.domain.user.domain.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -13,6 +14,8 @@ import java.util.UUID;
 public interface BoardRepository extends JpaRepository<Board, Long> {
     Optional<Board> findByIdAndUser(UUID boardId, User user); // 본인 게시글만 삭제 가능하도록
     Optional<Board> findById(UUID id); //ID 조회
-    Slice<Board> findByCategoryNameOrderByCreatedAtDesc(Category category, Pageable pageable); //카테고리 최신순
+    @EntityGraph(attributePaths = {"user"}) //N+1문제 해결
+    Slice<Board> findByCategoryNameOrderByCreatedAtDesc(Category category, Pageable pageable);
+
 
 }
--- src/main/java/capstone/mju/backend/domain/board/entity/repository/CommentRepository.java
+++ src/main/java/capstone/mju/backend/domain/board/entity/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package capstone.mju.backend.domain.board.entity.repository;
 import capstone.mju.backend.domain.board.entity.Board;
 import capstone.mju.backend.domain.board.entity.Comment;
 import capstone.mju.backend.domain.user.domain.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    @EntityGraph(attributePaths = {"user", "children", "children.user"})
     List<Comment> findByBoardIdOrderByCreatedAtAsc(UUID boardId);
     Optional<Comment> findByIdAndUser(UUID id, User user); // 본인 확인
     int countByBoard(Board board);
