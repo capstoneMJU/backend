dc64385a949a44b5dabaf4ecdec37757656caa1e
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-3.local>
2025-05-02 12:09:52 +0900

    feat: LoginData 추가 - #45

---
+++ src/main/java/capstone/mju/backend/domain/user/dto/response/LoginData.java
@@ -0,0 +1,15 @@
+package capstone.mju.backend.domain.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginData {
+    private String accessToken;
+}
