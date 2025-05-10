be5890c4edd0a88dfdfaa11f39ab4857e10fe9d8
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-3.local>
2025-05-10 15:33:50 +0900

    feat: SecurityConfig 수정 - #45

--- src/main/java/capstone/mju/backend/global/auth/swagger/SecurityConfig.java
+++ src/main/java/capstone/mju/backend/global/auth/swagger/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                         .requestMatchers("/posts/**").permitAll()
                         .requestMatchers("/api/v1/recipes/**").permitAll()
                         .requestMatchers("/api/v1/ocr/**").permitAll()
+                        .requestMatchers("/api/v1/user/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(config -> config.disable())
