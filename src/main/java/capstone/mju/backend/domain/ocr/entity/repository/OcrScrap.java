c6be1b87fab4c09f9bb2183637449b6fe75c18b9
이지수 <0801ljs@naver.com>
2025-05-13 00:10:43 +0900

    [FEAT] OcrScrap엔티티 생성

---
+++ src/main/java/capstone/mju/backend/domain/ocr/entity/repository/OcrScrap.java
@@ -0,0 +1,23 @@
+package capstone.mju.backend.domain.ocr.entity.repository;
+
+import capstone.mju.backend.domain.common.BaseEntity;
+import capstone.mju.backend.domain.nutrition.entity.FoodNutrition;
+import capstone.mju.backend.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "ocr_scrap",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "food_nutrition_id"}))
+public class OcrScrap extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_nutrition_id", nullable = false)
+    private FoodNutrition foodNutrition;
+}
